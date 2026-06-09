d2cd76abdf44e320ba9ef19f81ea0d3d85cab0b9
Victor Faurschou <mail@victorfaurschou.com>
2026-06-09 19:08:41 +0200

    Unarchived generated Fabric template

---
+++ src/client/java/com/victorfaurschou/storagecontainerlabels/client/StorageContainerLabelsClient.java
@@ -0,0 +1,10 @@
+package com.victorfaurschou.storagecontainerlabels.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StorageContainerLabelsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
